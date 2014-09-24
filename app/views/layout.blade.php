c448e0840e35d8a08129340806bc72406df099c2
evalqui1 <emilio.valqui@gmail.com>
2014-09-23 23:05:33 -0400

    fixed the layout

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html lang="en-us">
 	<head>
+		{{ HTML::style('css/main.css'); }}
 		<meta charset="utf-8">
 		<title>@yield('title')</title>
 	</head>
 	<body>
-		<ul>
-			<li><a href="/home">Home</a></li>
-			<li><a href="/about">About</a></li>
-			<li><a href="/services">Services</a></li>
-			<li><a href="/contact">Contact</a></li>
-		</ul>
+		<div id="menu">
+			<ul class="nav-menu">
+				<li><a href="/home">Home</a></li>
+				<li><a href="/about">About</a></li>
+				<li><a href="/services">Services</a></li>
+				<li><a href="/contact">Contact</a></li>
+			</ul>
+		</div>
 		@yield('content')
 	</body>
+
+
 </html>
